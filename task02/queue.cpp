14f0378bfd9d7676ae57d8a46d9963f54d40817d
jacknondaniels <voinkova.vo@phystech.edu>
2019-03-24 20:28:55 +0300

    template queue

--- task02/queue.cpp
+++ task02/queue.cpp
@@ -1,89 +1,120 @@
 //
 // Created by jacknondaniels on 21.03.19.
 //
-
 #include <iostream>
 #include "queue.h"
 
-Note::Note (data a):
-body (a),
-next (nullptr)
-{}
-
-
-ListQueue::ListQueue():
- head (nullptr),
- size (0)
-{}
-
-ListQueue::~ListQueue() {if (head) {
-        Note *current;
-        while (head->getnext())
-        {
-            current = head;
-            head = current->getnext();
-            delete current;
-        };
-        delete head;
-        std::cout << "Deleting the notes in list" << std::endl;
-    }
-}
-
-void ListQueue::enqueue(data & a) {
-    if (head)
-    {
-        Note *current = head;
-        while (current->getnext())
-            current = current->getnext();
-        current->setnext(new Note (a));
-    }
-    else if (!(head = new Note (a) ))
-        throw ("no space available");
-    size++;
-}
-
-data ListQueue::dequeue() {
-    if (getLength() <= 0)
-        throw ("i'm afraid i got nothing to show");
-    else {
-        data a = head->getdata();
-        Note *ptr = head;
-        head = head->getnext();
-        delete ptr;
-        size--;
-        return a;
-    }
-}
-
-ArrayQueue::ArrayQueue(unsigned int max_size):
- m_size(max_size),
- current_number(0),
- size (0)
+template <typename T>  class Note
 {
-    array = new data [m_size];
-    std::cout<< "i'm built!!!"<< std::endl;
-}
+public:
+    void setnext (Note *ptr) {next = ptr;}
+    Note *getnext () {return next;}
+    T &getdata () {return body;}
+    Note (T a = 0):
+            body (a),
+            next (nullptr)
+    {};
+private:
+    T body;
+    Note * next;
+};
 
-ArrayQueue::~ArrayQueue() {
-    delete [] array;
-    current_number = 0;
-    size = 0;
-    std::cout<< "let me fade away"<< std::endl;
-}
+template <typename T>  class ListQueue: public BasicQueue <T>
+{
+public:
+    ListQueue ():
+            head (nullptr),
+            size (0)
+    {};
 
-data ArrayQueue::dequeue() {
-    if (getLength() <= 0)
-        throw ("i'm afraid i got nothing to show");
-    else
-        return array[( current_number - (size--) ) % m_size];}
+    ~ListQueue() final
+    {if (head) {
+            Note<T> *current;
+            while (head->getnext())
+            {
+                current = head;
+                head = current->getnext();
+                delete current;
+            };
+            delete head;
+        }
+    };
 
-void ArrayQueue::enqueue(data &new_one) {
-    if (size >= m_size)
-            throw ("it's too much, huh");
-    else{
-        array[current_number % m_size] = new_one;
-        current_number++;
+    void enqueue (T&a) override  {
+        if (head)
+        {
+            Note<T> *current = head;
+            while (current->getnext())
+                current = current->getnext();
+            current->setnext(new Note<T> (a));
+            if (!current->getnext())
+                throw ("no space available");
+        }
+        else if (!(head = new Note<T> (a) ))
+            throw ("no space available");
         size++;
-    }
+    };
 
-}
+    T dequeue () override  {
+        if (getLength() <= 0)
+            throw ("no elements in queue");
+        else {
+            T a = head->getdata();
+            Note<T> *ptr = head;
+            head = head->getnext();
+            delete ptr;
+            size--;
+            return a;
+        }
+    };
+
+    unsigned getLength() override {return size;}
+
+private:
+    Note <T> *head;
+    unsigned size;
+};
+
+template <typename T>  class ArrayQueue: public BasicQueue <T>
+{
+public:
+    ArrayQueue(unsigned max_size = 64):
+        m_size(max_size),
+        current_number(0),
+        size (0)
+    {
+        if (!(array = new T [m_size]))
+            throw ("no space available");
+    };
+
+    ~ArrayQueue() final  {
+        delete [] array;
+        current_number = 0;
+        size = 0;
+    };
+
+    void enqueue (T& new_one) override  {
+        if (size >= m_size)
+            throw ("it's too much, huh");
+        else{
+            array[current_number % m_size] = new_one;
+            current_number++;
+            size++;
+        }
+    };
+
+    T dequeue () override{
+        if (getLength() <= 0)
+            throw ("no elements in queue");
+        else
+            return array[( current_number - (size--) ) % m_size];
+    };
+
+    unsigned getLength () override {return size;};
+
+private:
+    T *array;
+    unsigned m_size;
+    unsigned current_number;
+    unsigned size;
+};
