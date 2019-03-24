f7a654b220db2160c308c20727000e282b8731f9
jacknondaniels <voinkova.vo@phystech.edu>
2019-03-24 20:27:35 +0300

    template queue

--- task02/main.cpp
+++ task02/main.cpp
@@ -1,37 +1,57 @@
 
 #include <iostream>
+#include "queue.cpp"
 #include "queue.h"
 
 using std::cout, std::cin, std::endl, std::string;
 
 int main()
 {
-    unsigned n;
-    cout << "Number of elements in a queue: ";
-    cin >> n;
-
+    typedef string data;
     char c;
     cout << "Type of queue (a - array, l - list): ";
     cin >> c;
     
-    BasicQueue *queue = nullptr;
-    if (c == 'a')
-        queue = new ArrayQueue (n);
-    else if (c == 'l')
-        queue = new ListQueue;
-    else {
-        cout << "Bad type, exit" << endl;
-        return 1;
-    }
-
-
-
-    int i=0;
+    BasicQueue <data>  *queue = nullptr;
     try {
-        for (i; i<n; i++)
+        if (c == 'a') {
+            cout << "Size of array: ";
+            unsigned n;
+            cin >> n;
+            queue = new ArrayQueue <data> (n);
+        }
+        else if (c == 'l')
+            queue = new ListQueue<data>;
+        else {
+            cout << "Bad type, exit" << endl;
+            return 1;
+        }
+    }
+    catch (char const * error){
+        cout<<error<<endl;
+        exit (1);
+    };
+
+
+    data d;
+    cout << "Action (i - insert, t - take from queue, l - length of a queue, 0 - exit):"<< endl;
+    cin>>c;
+    try {
+        while (c)
         {
-            queue->enqueue(i);
-            cout<< queue->dequeue()<<endl;
+            if (c == 'i'){
+                cin>>d;
+                queue->enqueue(d);
+            }
+            else if (c == 't')
+                cout<<queue->dequeue()<<endl;
+            else if (c == 'l')
+                cout<<queue->getLength()<<endl;
+            else if (c == '0')
+                break;
+            else
+                cout<< "Bad type, try again"<<endl;
+            cin>>c;
         }
     }
     catch (char const* error){
@@ -39,9 +59,6 @@ int main()
             delete queue;
             exit (1);}
 
-    /*
-     * Put here your tests: en/dequeue large number of elements
-     */
     delete queue;
     return 0;
 }
--- task02/queue.h
+++ task02/queue.h
@@ -1,77 +1,17 @@
 
 #pragma once
 
-typedef int data;
-
-class BasicQueue
+template <typename T>
+        class BasicQueue
 {
 public:
-
-    // Add element to the end
-    virtual void enqueue(data &) = 0;
-
-    // Return first element
-    virtual data dequeue() = 0;
-
-    // Get total count of elements
+    virtual void enqueue(T &) = 0;
+    virtual T dequeue() = 0;
     virtual unsigned getLength() = 0;
     virtual ~BasicQueue() {};
 };
 
-class Note
-{
-public:
-    void setnext (Note *ptr) {next = ptr;}
-    Note *getnext () {return next;}
-    data &getdata () {return body;}
-    Note (data a = 0);
-
-private:
-    data body;
-    Note * next;
-};
-
-class ListQueue: public BasicQueue
-{
-public:
-    ListQueue();
-    ~ListQueue() final;
-
-    void enqueue (data&) override;
-    data dequeue () override;
-    unsigned getLength() override {return size;}
-
-private:
-    Note *head;
-    unsigned size;
-    /*
-     * Fully implement class:
-     * - declarations here
-     * - definitions in queue.cpp
-     * Could have infinite size
-     */
-};
-
-class ArrayQueue: public BasicQueue
-{
-public:
-    ArrayQueue(unsigned max_size = 64);
-    ~ArrayQueue() final;
-
-    void enqueue (data&) override;
-    data dequeue () override;
-    unsigned getLength () override {return size;};
-
-private:
-    data *array;
-    unsigned m_size;
-    unsigned current_number;
-    unsigned size;
-    /*
-     * Fully implement class:
-     * - declarations here
-     * - definitions in queue.cpp
-     * Only finite size implementation is required
-     */
-};
+template <typename T>  class Note;
+template <typename T>  class ListQueue;
+template <typename T>  class ArrayQueue;
 
