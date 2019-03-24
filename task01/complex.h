73cb870cd975e3975114793e645de6c603cb2f57
IM822 <48887605+IM822@users.noreply.github.com>
2019-03-24 23:41:51 +0300

    Update complex.h

--- task01/complex.h
+++ task01/complex.h
@@ -1,70 +1,61 @@
 #pragma once
-#include <iostream> //библиотека для работы с функциями ввода и вывода
+#include <iostream> 
 
-class Complex         // реализация класса комплексных чисел
+class Complex        
 {
-	// поля
-
-	//"личные" данные
 private:
-	float re, im;      // действительная и мнимая части
+	float re, im;      
 
 public:
-	// выбор конструктора осуществляется по правилам перегрузки
-
-	// конструктор по умолчанию 
+	
 	Complex();
 
-	// конструктор для вещественного числа
+	
 	Complex(float r);
 
-	// общий конструктор
+	
 	Complex(float r, float i);
 
-	// конструктор копирования
+	
 	Complex(const Complex &c);
 
-	// деструктор (для освобождения памяти)
+	
 	~Complex();
 
-	// модуль комплексного числа
+	
 	float abs();
 
-	//комплексно сопряженное выражение
+	
 	float conj();
 
-	// перегрузка оператора присваивания
+	
 	Complex& operator = (const Complex &c);
 
-	// перегрузка оператора +=
+	
 	Complex& operator += (const Complex &c);
 
-	// перегрузка оператора сложения
+	
 	Complex operator + (const Complex &c);
 
-	// перегрузка оператора вычитания
+	
 	Complex operator - (const Complex &c);
 
 	Complex operator*(const Complex & c);
 
-	// перегрузка оператора умножения
+	
 	Complex operator *= (const int a);
 
 
-	// перегрузка оператора деления
+	
 	Complex operator / (const Complex &c);
 
-	// укажем дружественные операторы, которым мы разрешаем доступ
-	// к "личным" (private) данным
 	friend std::ostream & operator<< (std::ostream &, const Complex &);
 	friend std::istream & operator>> (std::istream &, Complex &);
 
 };
 
-// перегрузка оператора <<
 std::ostream& operator<< (std::ostream &out, const Complex &c);
 
-// перегрузка оператора >>
 std::istream& operator>> (std::istream &in, Complex &c);
 
 Complex operator * (const Complex c, const int a);
