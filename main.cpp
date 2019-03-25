6f57b8225cb9f17ca465fa3b30d86d6c29c980ec
PotencialGibsa <48430457+PotencialGibsa@users.noreply.github.com>
2019-03-25 12:59:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include "queue.h"
+#include "queue.cpp"
+
+#define N  50
+
+
+int main()
+{
+    char c;
+    std::cout << "Type of queue (a - array, l - list): ";
+    std::cin >> c;
+
+    BasicQueue *queue = NULL;
+    if (c == 'a')
+        queue = new ArrayQueue;
+    else if (c == 'l')
+        queue = new ListQueue;
+    else {
+        std::cout << "Bad type, exit" << std::endl;
+        return 1;
+    }
+    int j;
+    data&j1=j;
+    for (j= 0 ; j <N; j++) {
+        std::cout<<j<<std::endl;
+
+        queue->enqueue(j);
+    }
+    std::cout<<" "<<std::endl;
+    int len = queue->getLength();
+    for (j= 0 ; j <len; j++) {
+        std::cout<<queue->dequeue()<<std::endl;
+    }
+    delete queue;
+    return 0;
+}
+
---
+++ queue.h
@@ -0,0 +1,55 @@
+#include <iostream>
+#pragma once
+
+typedef int data;
+
+class BasicQueue
+{
+public:
+
+    // Add element to the end
+    virtual void enqueue(data &) = 0;
+
+    // Return first element
+    virtual data dequeue() = 0;
+
+    // Get total count of elements
+    virtual unsigned getLength() = 0;
+
+};
+
+class ListQueue: public BasicQueue
+{
+public:
+    ListQueue();
+    void enqueue(data &);
+    data dequeue() ;
+    unsigned getLength();
+
+private:
+    struct Node {
+    Node () {
+        m_next = NULL;
+    }
+    Node(const data& t ) {
+        m_t = t;
+        m_next = NULL;
+    }
+    data m_t;
+    Node* m_next;
+};
+    Node*m_head;
+};
+
+class ArrayQueue: public BasicQueue
+{
+public:
+    ArrayQueue(unsigned max_size = 64);
+     void enqueue(data &);
+    data dequeue() ;
+    unsigned getLength();
+private:
+    data *ar;
+    int fr,re;
+};
+
