d0f5d565992bb421f2da5cb0ce703072fc476b22
vasil150 <48894159+vasil150@users.noreply.github.com>
2019-04-06 21:49:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
-#include "queue.h"
 #include <iostream>
+#include "queue.h"
 using namespace std;
-int main()
+
+ int main()
 {
-	int c, b;
-	data a[12], d;
-	ArrayQueue q(20);
-	cout << "press 1 if you want ArrayQueue, press 2 if you want ListQueue";
-	cin >> b;
-	if (b == 2)
-		ListQueue q;
-	for (c = 0;c < 10;c++)
+    char c;
+    std::cout << "Type of queue (a - array, l - list): ";
+    std::cin >> c;
+
+     BasicQueue *queue = NULL;
+    if (c == 'a')
+        queue = new ArrayQueue;
+    else if (c == 'l')
+        queue = new ListQueue;
+    else {
+        std::cout << "Bad type, exit" << std::endl;
+        return 1;
+    }
+
+     for (c = 0;c < 10;c++)
 	{
 		cin >> d;
-		q.enqueue(d);
-	
+		(*queue).enqueue(d);
 	}
 	for (c = 0;c < 10;c++)
 	{
-		cout << q.denqueue() << " ";
+		cout << (*queue).denqueue() << " " << (*queue).getLength << endl;
 	}
+     delete queue;
+    return 0;
+}
+
+	
 }
