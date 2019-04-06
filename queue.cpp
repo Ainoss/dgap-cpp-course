35955de9d1266d72648204b3303d717c2a599b5a
vasil150 <48894159+vasil150@users.noreply.github.com>
2019-04-06 22:45:44 +0300

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -2,60 +2,60 @@
 #include "queue.h"
 
 using namespace std;
-	ArrayQueue::ArrayQueue(int n)
-	{
-		data * ar = new data[n];
-		this->dat = ar;
-		this->hade = 0;
-		this->tale = 1;
-		this->lar = n;
-		this->lon = 0;
-    	}
-  	void ArrayQueue::enqueue(data &c)
-	{	
-		int k = this->tale;
-		this->dat[k] = c;
-		this->tale = (this->tale + 1) % this->lar;
-		this->lon++;
-	}
-	data ArrayQueue::denqueue()
-	{
-		data x;
-		x = dat[(this->hade + 1)%this->lar] ;
-		this->hade = (this->hade + 1) % this->lar;
-		this->lon--;
-		return(x);
-	}	
-	unsigned ArrayQueue::getLength()
-	{
-		return(this->lon);
-	}
-	void ListQueue::enqueue(data &c)
-	{
-		  this->last->nod = new List;
-		  this->last->val = c;
-		  this->last = this->last->nod;
-		  this->lon++;
-	}
-	ListQueue::ListQueue()
-	{	
+ArrayQueue::ArrayQueue(int n)
+{
+	data * ar = new data[n];
+	this->dat = ar;
+	this->hade = 0;
+	this->tale = 1;
+	this->lar = n;
+	this->lon = 0;
+}
+void ArrayQueue::enqueue(data &c)
+{	
+	int k = this->tale;
+	this->dat[k] = c;
+	this->tale = (this->tale + 1) % this->lar;
+	this->lon++;
+}
+data ArrayQueue::denqueue()
+{
+	data x;
+	x = dat[(this->hade + 1)%this->lar] ;
+	this->hade = (this->hade + 1) % this->lar;
+	this->lon--;
+	return(x);
+}	
+unsigned ArrayQueue::getLength()
+{
+	return(this->lon);
+}
+void ListQueue::enqueue(data &c)
+{	
+	this->last->nod = new List;
+	this->last->val = c;
+	this->last = this->last->nod;
+	this->lon++;
+}
+ListQueue::ListQueue()
+{	
 		
-		this->first = new List;
-		this->last = this->first;
-		this->lon = 0;
-	}
-	data ListQueue::denqueue()
-	{
-		data x;
-		x = this->first->val;
-		this->first = this->first->nod;
-		this->lon--;
-		return(x);
-	}
-	unsigned ListQueue::getLength()
-	{
-		return(this->lon);
-	}
+	this->first = new List;
+	this->last = this->first;
+	this->lon = 0;
+}
+data ListQueue::denqueue()
+{
+	data x;
+	x = this->first->val;
+	this->first = this->first->nod;
+	this->lon--;
+	return(x);
+}
+unsigned ListQueue::getLength()
+{
+	return(this->lon);
+}
 	
 	
 
