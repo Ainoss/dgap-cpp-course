0f55b2e3e4f573ce1a379cedace5a766abb33145
jacknondaniels <voinkova.vo@phystech.edu>
2019-04-13 15:03:50 +0300

    template implementation made in a header. Class Note definition made inside of class ListQueue. Pointer to the last member of a ListQueue added.

--- task02/main.cpp
+++ task02/main.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include "queue.cpp"
 #include "queue.h"
 
 using std::cout, std::cin, std::endl, std::string;
@@ -8,54 +7,54 @@ using std::cout, std::cin, std::endl, std::string;
 int main()
 {
     typedef string data;
-    char c;
+    string c;
     cout << "Type of queue (a - array, l - list): ";
     cin >> c;
     
     BasicQueue <data>  *queue = nullptr;
     try {
-        if (c == 'a') {
+        if (c == "a") {
             cout << "Size of array: ";
             unsigned n;
             cin >> n;
             queue = new ArrayQueue <data> (n);
         }
-        else if (c == 'l')
+        else if (c == "l")
             queue = new ListQueue<data>;
         else {
             cout << "Bad type, exit" << endl;
             return 1;
         }
     }
-    catch (char const * error){
-        cout<<error<<endl;
+    catch (std::exception &e){
+        cout << e.what() << endl;
         exit (1);
     };
 
 
     data d;
     cout << "Action (i - insert, t - take from queue, l - length of a queue, 0 - exit):"<< endl;
-    cin>>c;
+    cin >> c;
     try {
-        while (c)
+        while (c != "0")
         {
-            if (c == 'i'){
-                cin>>d;
+            if (c == "i"){
+                cin >> d;
                 queue->enqueue(d);
             }
-            else if (c == 't')
-                cout<<queue->dequeue()<<endl;
-            else if (c == 'l')
-                cout<<queue->getLength()<<endl;
-            else if (c == '0')
+            else if (c == "t")
+                cout << queue->dequeue() << endl;
+            else if (c == "l")
+                cout << queue->getLength() << endl;
+            else if (c == "0")
                 break;
             else
-                cout<< "Bad type, try again"<<endl;
-            cin>>c;
+                cout << "Bad type, try again" << endl;
+            cin >> c;
         }
     }
-    catch (char const* error){
-            cout<< error<< endl;
+    catch (std::exception &e){
+            cout << "error caught: " << e.what() << endl;
             delete queue;
             exit (1);}
 
--- task02/queue.h
+++ task02/queue.h
@@ -1,5 +1,7 @@
 
 #pragma once
+#include <iostream>
+#include <stdexcept>
 
 template <typename T>
         class BasicQueue
@@ -11,7 +13,118 @@ public:
     virtual ~BasicQueue() {};
 };
 
-template <typename T>  class Note;
-template <typename T>  class ListQueue;
-template <typename T>  class ArrayQueue;
+template <typename T>
+        class ListQueue: public BasicQueue <T> {
+public:
+    ListQueue() :
+            head(nullptr),
+            size(0),
+            tail(nullptr) {};
+
+    ~ListQueue() final {
+        if (head) {
+            Note *current;
+            while (head->getnext()) {
+                current = head;
+                head = current->getnext();
+                delete current;
+            };
+            delete head;
+        }
+    };
+
+    void enqueue(T &a) override {
+        if (tail) {
+            tail->setnext(new Note(a));
+            tail = tail->getnext();
+        } else {
+            head = new Note(a);
+            tail = head;
+        }
+        size++;
+    };
+
+    T dequeue() override {
+        if (getLength() <= 0)
+            throw (std::runtime_error("no elements in queue"));
+        else {
+            T a = head->getdata();
+            Note *ptr = head;
+            head = head->getnext();
+            delete ptr;
+            size--;
+            return a;
+        }
+    };
+
+    unsigned getLength() override { return size; }
+
+private:
+    class Note {
+    public:
+        void setnext(Note *ptr) { next = ptr; }
+
+        Note *getnext() { return next; }
+
+        T &getdata() { return body; }
+
+        explicit Note(T a = 0) :
+                body(a),
+                next(nullptr) {};
+    private:
+        T body;
+        Note *next;
+    };
+
+    Note *head;
+    Note *tail;
+    unsigned size;
+
+};
+
+
+
+template <typename T>
+        class ArrayQueue: public BasicQueue <T> {
+
+
+public:
+    explicit ArrayQueue (unsigned given_size = 64):
+        max_size(given_size),
+        current_number(0),
+        size (0),
+        array (new T [given_size]) {};
+
+    ~ArrayQueue() final  {
+        delete [] array;
+        current_number = 0;
+        size = 0;
+    };
+
+    void enqueue (T& new_one) override  {
+        if (size >= max_size)
+            throw (std::runtime_error("it's too much, huh"));
+        else{
+            array[current_number % max_size] = new_one;
+            current_number++;
+            size++;
+        }
+    };
+
+    T dequeue () override{
+        if (getLength() <= 0)
+            throw (std::runtime_error("no elements in queue"));
+        else
+            return array[( current_number - (size--) ) % max_size];
+        };
+
+    unsigned getLength () override {return size;}
+
+
+private:
+        T *array;
+        const unsigned max_size;
+        unsigned current_number;
+        unsigned size;
+};
 
