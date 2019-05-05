fc37fb1829185e1b44ca1dbf88746d816ee101a2
Dmitriy <43829114+LordCalm@users.noreply.github.com>
2019-05-06 00:34:35 +0300

    Add files via upload

--- task04/storage.h
+++ task04/storage.h
@@ -1,16 +1,46 @@
-
+#pragma once
+#include <string.h>
+#pragma warning (disable : 4996)
 
 template <typename T>
 class Storage
 {
 public:
-    Storage() = default;
-    Storage(const T &val);
-    ~Storage();
+	Storage() = default;
+	Storage(const T& val) : m_val(val) {};
+	~Storage() = default;
 
-    void SetVal(const T &val);
-    T GetVal();
+	void SetVal(const T& val) {
+		m_val = val;
+	};
+	T GetVal() {
+		return m_val;
+	};
 private:
-    T m_val;
+	T m_val;
 };
 
+template <>
+class Storage<char*>
+{
+public:
+	Storage() = default;
+	Storage(const char* val) {
+		m_val = new char[strlen(val) + 1];
+		strcpy(m_val, val);
+	};
+	~Storage() {
+		delete[] m_val;
+	};
+
+	void SetVal(const char* val) {
+		delete[] m_val;
+		m_val = new char[strlen(val) + 1];
+		strcpy(m_val, val);
+	};
+	char* GetVal() {
+		return m_val;
+	};
+private:
+	char* m_val;
+};
