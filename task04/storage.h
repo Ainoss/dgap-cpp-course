f8b8572107243a642228e52047ebcd1c3c01fecc
jacknondaniels <voinkova.vo@phystech.edu>
2019-05-08 18:50:04 +0300

    Task 4: specialization for a template

--- task04/storage.h
+++ task04/storage.h
@@ -1,3 +1,6 @@
+#pragma once
+#include <iostream>
+#include <cstring>
 
 
 template <typename T>
@@ -5,12 +8,46 @@ class Storage
 {
 public:
     Storage() = default;
-    Storage(const T &val);
-    ~Storage();
+    Storage(const T &val):
+    m_val(val)
+    {};
+    ~Storage() = default;
 
-    void SetVal(const T &val);
-    T GetVal();
+    void SetVal(const T &val){
+        m_val = val;
+    };
+    T GetVal(){
+        return m_val;
+    };
 private:
     T m_val;
 };
 
+template <> class Storage<char*> {
+
+private:
+    char *m_val;
+    unsigned m_size;
+public:
+    Storage (const char* val)
+    {
+        m_size = strlen(val);
+        m_val = new char [m_size+1];
+        strcpy(m_val, val);
+    };
+    ~Storage(){
+        delete[] m_val;
+    };
+    void SetVal (const char* val){
+        unsigned n = strlen(val);
+        if (n > m_size) {
+            m_size = n;
+            delete[] m_val;
+            m_val = new char[n+1];
+        }
+       strcpy(m_val, val);
+    };
+    char* GetVal(){
+        return m_val;
+    };
+};
