68eb72338b928d95bda6372969a99646c0af0b57
Yuriy Tatarenko <tatarenko_ura@mail.ru>
2019-05-14 16:46:00 +0300

    new commit

--- task04/storage.h
+++ task04/storage.h
@@ -4,46 +4,46 @@ template <typename T>
 class Storage
 {
 public:
-	Storage() = default;
-	Storage(const T &val): m_val(val) {};
-	~Storage(){}
+    Storage() = default;
+    Storage(const T &val): m_val(val) {};
+    ~Storage(){}
 
-	void SetVal(const T &val)
-	{
-		m_val = val;
-	}
-	T GetVal()
-	{
-		return m_val;
-	}
+    void SetVal(const T &val)
+    {
+    	m_val = val;
+    }
+    T GetVal()
+    {
+    	return m_val;
+    }
 private:
-	T m_val;
+    T m_val;
 };
 
 template <>
 class Storage<char *>
 {
 public:
-	Storage() = default;
-	Storage(const char* val)
-	{
-		m_val = (char*)malloc(sizeof(val));
-		strcpy(m_val, val);
-	}
-	~Storage()
-	{
-		free(m_val);
-	}
+    Storage() = default;
+    Storage(const char* val)
+    {
+    	m_val = (char*)malloc(sizeof(val));
+    	strcpy(m_val, val);
+    }
+    ~Storage()
+    {
+    	free(m_val);
+    }
 
-	void SetVal(const char* val)
-	{
-		realloc(m_val, sizeof(val));
-		strcpy(m_val, val);
-	}
-	char* GetVal()
-	{
-		return m_val;
-	}
+    void SetVal(const char* val)
+    {
+    	realloc(m_val, sizeof(val));
+    	strcpy(m_val, val);
+    }
+    char* GetVal()
+    {
+    	return m_val;
+    }
 private:
-	char* m_val;
+    char* m_val;
 };
