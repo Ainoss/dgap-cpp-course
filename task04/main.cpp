54e8b486cd195cbf5d7511a806c8340e257d0ee8
EtoPashka <osmanov.pa@phystech.edu>
2019-05-19 23:28:00 +0300

    Османов Павел. Задание 4

---
+++ task04/main.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <string>
+#include "storage.h"
+
+using namespace std;
+
+
+int main()
+{
+    int i = 33;
+
+    // With int it works very well, holding actual value:
+    Storage<int> st_int(i);
+    cout << "Stored int " << st_int.GetVal() << endl;
+    cout << "New val: ";
+    cin >> i;
+    st_int.SetVal(i);
+    cout << "Stored int " << st_int.GetVal() << endl;
+
+    // But with usual char* it doesn't work:
+    char str[100] = "Hello";
+    Storage<char*> st_str(str);
+    cout << "Stored str " << st_str.GetVal() << endl;
+    str[1] = 'X';
+
+    // Value in storage broken:
+    cout << "Stored str " << st_str.GetVal() << endl;
+
+    st_str.SetVal("Bye, have a great time");
+    cout << "Stored str " << st_str.GetVal() << endl;
+
+
+    return 0;
+}
